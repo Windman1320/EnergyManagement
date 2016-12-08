226ac2f46a7b65e73fcb97f64949f70d4eda25fd
cappuccinoqi <617674934@qq.com>
2016-12-08 22:47:04 +0800

    sdk改为23

--- app/src/main/java/com/tony/energymanagement/em/LoginActivity.java
+++ app/src/main/java/com/tony/energymanagement/em/LoginActivity.java
@@ -3,10 +3,10 @@ package com.tony.energymanagement.em;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.tony.energymanagement.energymanagement.R;
 
@@ -26,21 +26,26 @@ public class LoginActivity extends AppCompatActivity {
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if("".equals(ed_username.getText())|ed_username.getText()==null) {
-                    Toast.makeText(LoginActivity.this,"用户名不能为空！",Toast.LENGTH_SHORT).show();
-                    if("".equals(ed_passwd.getText())||ed_passwd.getText()==null){
-                        Toast.makeText(LoginActivity.this,"密码不能为空！",Toast.LENGTH_SHORT).show();
-                    }else {
-                        Intent intent = new Intent(LoginActivity.this,Main.class);
-                        startActivity(intent);
-                    }
-                }
+            public void onClick(View v) {
+               Log.i("LoginActivity:","onClick執行了");
+               Log.i("LoginActivity:","username->"+ed_username.getText());
+               Log.i("LoginActivity:","username->"+ed_passwd.getText());
+
+
+//                if("".equals(ed_username.getText())||ed_username.getText()==null) {
+//                    Toast.makeText(LoginActivity.this,"用户名不能为空！",Toast.LENGTH_SHORT).show();
+//                    if("".equals(ed_passwd.getText())||ed_passwd.getText()==null){
+//                        Toast.makeText(LoginActivity.this,"密码不能为空！",Toast.LENGTH_SHORT).show();
+//                    }else {
+//
+//                    }
+//                }
                 //验证用户名和密码是否正确
 
 
                 //跳转到下一个activity
-
+                Intent intent = new Intent(LoginActivity.this,Main.class);
+                startActivity(intent);
             }
         });
     }
