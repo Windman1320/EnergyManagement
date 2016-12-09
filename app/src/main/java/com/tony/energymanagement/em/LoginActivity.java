e91c11b5434c0ed79fd8400e20cfdcb32a5cd5d5
e <zaiqingyang@gmail.com>
2016-12-09 17:01:26 +0800

    登录网络验证初步实现-2

--- app/src/main/java/com/tony/energymanagement/em/LoginActivity.java
+++ app/src/main/java/com/tony/energymanagement/em/LoginActivity.java
@@ -8,8 +8,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.tony.energymanagement.energymanagement.R;
+
+import org.zaiqingyang.energymanagement.utils.LoginVerify;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText ed_username;
@@ -26,34 +29,54 @@ public class LoginActivity extends AppCompatActivity {
 
 
         btn_login.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
+                String username = ed_username.getText().toString();
+                String password = ed_passwd.getText().toString();
                Log.i("LoginActivity:","onClick執行了");
                Log.i("LoginActivity:","username->"+ed_username.getText());
                Log.i("LoginActivity:","username->"+ed_passwd.getText());
+                /**
+                 *
+                 */
+                class LoginRunnable implements Runnable{
+                    private String username;
+                    private String password;
+                    public LoginRunnable(String username,String password){
+                        this.username = username;
+                        this.password = password;
+                    }
+                    @Override
+                    public void run() {
+                        if(LoginVerify.login(username,password)){
+                            //跳转到下一个activity
+                            Intent intent = new Intent(LoginActivity.this,Main.class);
+                            startActivity(intent);
+                        }else{
+                            Toast.makeText(LoginActivity.this,"用户名或者密码错误!",Toast.LENGTH_SHORT).show();
+                        }
 
-
-                if("".equals(ed_username.getText().toString())||ed_username.getText()==null) {
+                    }
+                }
+                if(username==null||"".equals(username)) {
+                    Log.d("test",username);
                     //弹出对话框提示用户
                     new AlertDialog.Builder(LoginActivity.this)
                             .setMessage("请输入用户名！")
                             .setPositiveButton("OK", null)
                             .show();
 
-                }else if("".equals(ed_passwd.getText().toString())||ed_passwd.getText()==null){
+                }else if(password==null||"".equals(password)){
                     new AlertDialog.Builder(LoginActivity.this)
                             .setMessage("密码不能为空！")
                             .setPositiveButton("OK", null)
                             .show();
                 }else {
-                    //跳转到下一个activity
-                    Intent intent = new Intent(LoginActivity.this,Main.class);
-                    startActivity(intent);
+
+                    new Thread(new LoginRunnable(username,password)).start();
+
                 }
-                //验证用户名和密码是否正确
-
-
-
             }
         });
     }
